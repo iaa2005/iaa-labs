9b9ffbf56ab8dcd1d9f95056af013c4593e2a4f4
IAA Labs <marsee3015@gmail.com>
2022-06-06 11:38:52 +0300

    Add files via upload

---
+++ index-2.js
@@ -0,0 +1,20 @@
+
+$(document).ready(async function() {
+
+    let date1 = new Date("06/06/2022");
+    let date2 = Date.now();
+    let Difference_In_Time = date2 - date1.getTime();
+    let Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+    let days = Math.floor(Difference_In_Days);
+
+    let delay = 0.5;
+    for (let i = 0; i < 300; i++) {
+        if (days <= 0) {
+            $(".calendar-div").append(`<div class="celum trans"></div>`);
+        } else {
+            $(".calendar-div").append(`<div class="celum" style="animation: opacity-00-100; animation-duration: 0.2s; animation-iteration-count: 1; animation-delay: ${delay}s; animation-fill-mode: forwards; animation-timing-function: ease-out;"></div>`);
+        }
+        delay += 0.05;
+        days--;
+    }
+});
---
+++ index.js
@@ -0,0 +1,50 @@
+$(document).ready(async function() {
+
+    window.addEventListener('scroll', function () {
+        $(".block-main").css("opacity", (1 - window.scrollY / window.screen.height));
+
+        if (window.scrollY / window.screen.height < 2) {
+            $(".block-1").css("opacity", window.scrollY / window.screen.height - 1);
+        } else {
+            $(".block-1").css("opacity", (3 - window.scrollY / window.screen.height));
+        }
+
+        if (window.scrollY / window.screen.height < 4) {
+            $(".block-2").css("opacity", window.scrollY / window.screen.height - 3);
+        } else {
+            $(".block-2").css("opacity", (5 - window.scrollY / window.screen.height));
+        }
+
+        if (window.scrollY / window.screen.height < 6) {
+            $(".block-3").css("opacity", window.scrollY / window.screen.height - 5);
+        } else {
+            $(".block-3").css("opacity", (7 - window.scrollY / window.screen.height));
+        }
+
+        if (window.scrollY / window.screen.height < 1) {
+            $(".block-main").removeClass("remove");
+        } else {
+            $(".block-main").addClass("remove");
+        }
+
+        if (window.scrollY / window.screen.height > 1 && window.scrollY / window.screen.height < 3) {
+            $(".block-1").removeClass("remove");
+        } else {
+            $(".block-1").addClass("remove");
+        }
+
+        if (window.scrollY / window.screen.height > 3 && window.scrollY / window.screen.height < 5) {
+            $(".block-2").removeClass("remove");
+        } else {
+            $(".block-2").addClass("remove");
+        }
+
+        if (window.scrollY / window.screen.height > 5 && window.scrollY / window.screen.height < 7) {
+            $(".block-3").removeClass("remove");
+        } else {
+            $(".block-3").addClass("remove");
+        }
+
+        console.log(window.scrollY / window.screen.height);
+    });
+});
