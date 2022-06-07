1d6dff8ecb9fc8ccafd4cf07ec40509437a587a0
iaa2005 <marsee3015@gmail.com>
2022-06-07 10:40:52 +0300

    -

--- index-2.js
+++ index-2.js
@@ -6,15 +6,20 @@ $(document).ready(async function() {
     let Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
     let days = Math.floor(Difference_In_Days);
 
-    let delay = 1;
-    for (let i = 0; i < 270; i++) {
-        if (days <= 0) {
-            $(".calendar-div").append(`<div class="celum trans"></div>`);
-        } else {
-            $(".calendar-div").append(`<div class="celum" style="animation: opacity-00-100; animation-duration: 0.2s; animation-iteration-count: 1; animation-delay: ${delay}s; animation-fill-mode: forwards; animation-timing-function: ease-out;"></div>`);
+    $.getJSON("../data.json", function (data) {
+        let tasks = data.tasks;
+        let delay = 1;
+        for (let i = 0; i < 270; i++) {
+            if (days <= 0) {
+                $(".calendar-div").append(`<div class="celum trans"></div>`);
+            } else {
+                let task_count;
+                if (tasks[i] === undefined) task_count = "";
+                else task_count = tasks[i];
+                $(".calendar-div").append(`<div class="celum" style="animation: opacity-00-100; animation-duration: 0.2s; animation-iteration-count: 1; animation-delay: ${delay}s; animation-fill-mode: forwards; animation-timing-function: ease-out;">${task_count}</div>`);
+            }
+            delay += 0.05;
+            days--;
         }
-        delay += 0.05;
-        days--;
-    }
-
+    })
 });
